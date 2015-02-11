56236a05cf9b8703bd471b2bc5b9a22f95455c53
Karsten Schmidt <k@postspectacular.com>
2015-02-11 13:53:53 +0000

    refactor :pool to store pre-styled numbers, add max-num constant

--- src/rndfarm/handler.clj
+++ src/rndfarm/handler.clj
@@ -9,24 +9,40 @@
    [ring.util.response :as resp]
    [clojure.java.io :as io]))
 
+(def max-num (bit-shift-left 1 62))
+
+(def formatter (java.text.DecimalFormat. "#,###,###,###,###,###,###"))
+
 (defn as-long
   [n]
   (try
     (Long/parseUnsignedLong n)
     (catch Exception e)))
 
+(defn style-number
+  [n & [cls]]
+  (let [h (nth [:h1 :h2 :h3 :h4 :h5] (long (rem n 5)))
+        col (Long/toString n 16)
+        col (subs col (max 0 (- (count col) 6)))
+        px (* 100 (/ (bit-and n 1023) 1047.0))
+        py (* 100 (/ (bit-and (unsigned-bit-shift-right n 10) 1023) 1047.0))]
+    [h {:class (if cls (str "rnd " cls) "rnd")
+        :style (format "color:#%s;left:%d%%;top:%d%%;" col (int px) (int py))} n]))
+
 (defn persist-number
   [state n]
-  (let [^java.io.Writer writer (:writer state)]
+  (let [^java.io.Writer writer (:writer state)
+        n' (style-number n)]
     (try
       (prn "adding number: " n)
       (.write writer (str n "\n"))
       (.flush writer)
       (-> state
+          (assoc :last n)
           (update-in [:count] inc)
           (update-in [:pool] #(if (< (count %) 100)
-                                (conj % n)
-                                (conj (subvec % 1) n))))
+                                (conj % n')
+                                (conj (subvec % 1) n'))))
       (catch Exception e
         (.printStackTrace e)
         state))))
@@ -42,24 +58,14 @@
     (prn "read file: " stream (count numbers))
     (agent
      {:writer (io/writer stream :append true)
-      :pool (mapv as-long (take-last 100 numbers))
+      :pool (mapv #(style-number (as-long %)) (take-last 100 numbers))
+      :last (as-long (last numbers))
       :count (count numbers)})))
-
-(def formatter (java.text.DecimalFormat. "#,###,###,###,###,###,###"))
-
-(defn style-number
-  [n & [cls]]
-  (let [h (nth [:h1 :h2 :h3 :h4 :h5] (long (rem n 5)))
-        col (Long/toString n 16)
-        col (subs col (max 0 (- (count col) 6)))
-        px (* 100 (/ (bit-and n 1023) 1047.0))
-        py (* 100 (/ (bit-and (unsigned-bit-shift-right n 10) 1023) 1047.0))]
-    [h {:class (if cls (str "rnd " cls) "rnd")
-        :style (format "color:#%s;left:%d%%;top:%d%%;" col (int px) (int py))} n]))
 
 (defroutes app-routes
   (GET "/" [:as req]
-       (html5 {:lang "en"}
+       (html5
+        {:lang "en"}
         [:head
          [:meta {:charset "utf-8"}]
          [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
@@ -82,19 +88,20 @@
              [:input {:type "number" :name "n"
                       :placeholder "your random number"
                       :autofocus true
-                      :min "0" :max "9223372036854775808"}]]
+                      :min "0" :max max-num}]]
             [:div.row [:input {:type "submit"}]]]
            [:div.row.row-footer
-            [:a {:href "#"} "About"]
-            " &middot; "
-            [:a {:href "#" :title "Access forthcoming"} "API"]
-            " &middot; "
-            [:a {:href "https://github.com/postspectacular/rnd.farm"} "GitHub"]
+            (interpose
+             " &middot; "
+             [#_[:a {:href "#"} "About"]
+              #_[:a {:href "#" :title "Access forthcoming"} "API"]
+              [:a {:href "https://github.com/postspectacular/rnd.farm"} "GitHub"]])
             [:br]
             " &copy; 2015 "
             [:a {:href "http://postspectacular.com"} "postspectacular.com"]]]]
-         (map style-number (butlast (:pool @store)))
-         (style-number (peek (:pool @store)) "rnd-last")]))
+         (butlast (:pool @store))
+         (style-number (:last @store) "rnd-last")]))
+
   (POST "/" [n]
         (if-let [n' (as-long n)]
           (do
@@ -103,6 +110,7 @@
                 (assoc :flash {:type :ok :msg (str "Thanks, that's a great number: " n')})))
           (-> (resp/redirect "/")
               (assoc :flash {:type :err :msg "Hmmm.... that number wasn't so good!"}))))
+
   (route/not-found "Not Found"))
 
 (def app
